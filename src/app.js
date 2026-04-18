8fae5cce434d9a5587851df7df478e2d5fd34c4e
rohankumar <=>
2026-04-19 01:13:49 +0530

    utils code discussion

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors({
     origin: config.BASE_URI,
-    options: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"]
 })
@@ -22,6 +22,7 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(cookieParser());
+app.use(express.static("public"));
 
 
 export default app;
