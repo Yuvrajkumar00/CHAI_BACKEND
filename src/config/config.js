8e225097cd2642c0a0f029b8668e251dccdf556c
rohankumar <=>
2026-04-20 22:06:59 +0530

    add user and video model

--- src/config/config.js
+++ src/config/config.js
@@ -13,10 +13,27 @@ if (!process.env.MONGODB_URI) {
     throw new Error("Missing MONGODB_URI in environment variables. Please define it in your .env file.");
 }
 
+if (!process.env.ACCESS_TOKEN_SECRET) {
+    throw new Error("Missing ACCESS_TOKEN_SECRET in environment variables. Please define it in your .env file.");
+}
+if (!process.env.ACCESS_TOKEN_EXPIRES_IN) {
+    throw new Error("Missing ACCESS_TOKEN_EXPIRES_IN in environment variables. Please define it in your .env file.");
+}
+if (!process.env.REFRESH_TOKEN_SECRET) {
+    throw new Error("Missing REFRESH_TOKEN_SECRET in environment variables. Please define it in your .env file.");
+}
+if (!process.env.REFRESH_TOKEN_EXPIRES_IN) {
+    throw new Error("Missing REFRESH_TOKEN_EXPIRES_IN in environment variables. Please define it in your .env file.");
+}
+
 const config = {
     PORT: process.env.PORT,
     BASE_URI: process.env.BASE_URI,
     MONGODB_URI: process.env.MONGODB_URI,
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+    ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN,
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+    REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN,
 }
 
 export default config;
---
+++ src/models/User.models.js
@@ -0,0 +1,94 @@
+import mongoose, { Schema } from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import config from '../config/config.js';
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+            lowercase: true,
+            index: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+            lowercase: true,
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+        avatar: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        coverImage: {
+            type: String,
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Video',
+            },
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required'],
+        },
+        refreshToken: {
+            type: String,
+        },
+    },
+    { timestamps: true }
+);
+
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = () => {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname,
+        },
+        config.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: config.ACCESS_TOKEN_EXPIRES_IN,
+        }
+    );
+};
+
+userSchema.methods.generateRefreshToken = () => {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        config.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: config.REFRESH_TOKEN_EXPIRES_IN,
+        }
+    );
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
