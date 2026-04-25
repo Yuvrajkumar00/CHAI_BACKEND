c91b1227defe3a65f37baea66876251cd7112c96
= <=>
2026-04-25 23:59:25 +0530

    fix register controller

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -4,20 +4,30 @@ import config from '../config/config.js';
 
 cloudinary.config({
     cloud_name: config.CLOUDINARY_CLOUD_NAME,
-    api_key: config,
+    api_key: config.CLOUDINARY_API_KEY,
     api_secret: config.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
+    console.log(localFilePath);
+    
     try {
         if (!localFilePath) return null;
         // upload file to cloudinary
         const response = await cloudinary.uploader.upload(localFilePath, {resource_type: 'auto'});
         console.log("file is uploaded on cloudinary", response.url);
+        fs.unlinkSync(localFilePath, (err) => { 
+            if(err) console.log("File delete error:", err);
+        });
         return response;
     }
     catch (error) {
-        fs.unlinkSync(localFilePath) // delete the local file if upload fails
+        console.error("Cloudinary Error:", error); // 👈 ADD THIS
+
+        fs.unlinkSync(localFilePath, (err) => {
+            if(err) console.log("File delete error:", err);
+        }) // delete the local file if upload fails
+
         return null;
     }
 };
